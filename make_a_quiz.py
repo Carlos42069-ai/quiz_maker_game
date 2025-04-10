fb52134e1d6ee5e02d4071591e41a54d8d58a150
Carlos <carlosroque117@gmail.com>
2025-04-10 11:25:21 +0800

    Pseudocode for the quiz maker im going to do.

---
+++ make_a_quiz.py
@@ -0,0 +1,19 @@
+#Start the program
+
+#Display a welcome message
+
+#Ask the user to give a question and choices:
+#    - Ask for the question.
+#    - Ask for the options (a, b, c, d).
+#    - Ask for the correct answer (a, b, c, or d).
+#    - Save the question, options, and correct answer to a text file (`questions.txt`).
+
+#After saving the quiz:
+#    - Display a message confirming the question has been saved.
+#    - Ask if the user wants to create another question or exit the program.
+
+#If the user chooses to exit:
+#    - Display a exiting message.
+#    - End the program after.
+
+#End
